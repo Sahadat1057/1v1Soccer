0cfa3366dd5cea05ea93c8d2c1f51d77df3b27ce
sahadatamzad <samzad7698@bths.edu>
2023-06-01 20:17:03 -0400

    made the labels visible by setting opaque to false, need to do animations + goal collision

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -9,29 +9,34 @@ public class GameGUI extends JFrame implements KeyListener {
     private JLabel netLabel;
 
     private JPanel panel;
+    private JLabel fieldLabel;
     private Ball ballSpecs;
     private Net netSpecs;
     public Image ballImage;
     public Image netImage;
-    public Image
+    public Image fieldImage;
 
 
     public GameGUI() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
+        this.setSize(650,300);
         this.setLayout(null);
         this.setVisible(true);
         this.addKeyListener(this);
 
+        fieldLabel = new JLabel();
+        fieldLabel.setBounds(0, 0, 650, 300);
+        fieldLabel.setOpaque(true);
+        this.add(fieldLabel);
+
+
         ballLabel = new JLabel();
-        ballLabel.setBounds(200, 400, 60, 60);
-        ballLabel.setOpaque(true);
-        this.add(ballLabel);
+        ballLabel.setBounds(325, 200, 60, 60);
+        ballLabel.setOpaque(false);
 
         netLabel = new JLabel();
-        netLabel.setBounds(0, 0, 100, 100);
-        netLabel.setOpaque(true);
-        this.add(netLabel);
+        netLabel.setBounds(325, 75, 100, 100);
+        netLabel.setOpaque(false);
 
 
         ImageIcon ball = new ImageIcon("src/Ball.png");
@@ -45,6 +50,16 @@ public class GameGUI extends JFrame implements KeyListener {
         Image newimg2 = image2.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
         net = new ImageIcon(newimg2);
         netLabel.setIcon(net);
+
+        fieldLabel.add(ballLabel);
+        fieldLabel.add(netLabel);
+
+        ImageIcon field = new ImageIcon("src/Field.png");
+        Image image3 = field.getImage();
+        Image newimg3 = image3.getScaledInstance(650, 300, Image.SCALE_SMOOTH);
+        field = new ImageIcon(newimg3);
+        fieldLabel.setIcon(field);
+
 
         ballSpecs = new Ball(10,400);
         netSpecs = new Net(10);
@@ -65,24 +80,27 @@ public class GameGUI extends JFrame implements KeyListener {
 
             case 32:
             /*
-             for (int i = 0; i < 10; i++) {
                  int speed = ballSpecs.getMaxSpeed() / 10;
                  ballLabel.setLocation(ballLabel.getX(), ballLabel.getY() - speed);
-             }
-             */
-             repaint();
                 break;
+                */
+
             }
 
         System.out.println(e.getKeyCode());
 
         }
 
+        /*
     public void paintComponent (Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
         g2D.drawImage( , 0, 0, null);
 
     }
+    */
+         public void keyReleased(KeyEvent e){
+
+         }
 
 
 
